07ade98ed3eaea08c95336029142519bd84b5ad8
dwrik <wrikdas22@gmail.com>
2025-12-12 00:33:14 +0530

    feat: implement mutex

--- src/main/java/com/dwrik/mutex/SimpleMutex.java
+++ src/main/java/com/dwrik/mutex/SimpleMutex.java
@@ -1,4 +1,61 @@
 package com.dwrik.mutex;
 
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.LockSupport;
+
 public class SimpleMutex {
+    private final AtomicInteger state;
+    private final AtomicInteger listLock;
+    private boolean hasWaiters;
+    private Waiter head;
+
+    public SimpleMutex() {
+        state = new AtomicInteger(0);
+        listLock = new AtomicInteger(0);
+    }
+
+    public void lock() {
+        while (true) {
+            if (state.compareAndSet(0, 1)) {
+                return;
+            } else {
+                while (!listLock.compareAndSet(0, 1)) {
+                    Thread.onSpinWait();
+                };
+                pushWaiter();
+                hasWaiters = true;
+                listLock.compareAndSet(1, 0);
+                LockSupport.park();
+            }
+        }
+    }
+
+    public void unlock() {
+        if (!hasWaiters) {
+            state.compareAndSet(1, 0);
+        } else {
+            while (!listLock.compareAndSet(0, 1)) {
+                Thread.onSpinWait();
+            };
+            Waiter waiter = popWaiter();
+            hasWaiters = head != null;
+            listLock.compareAndSet(1, 0);
+            state.compareAndSet(1, 0);
+            LockSupport.unpark(waiter.thread);
+        }
+    }
+
+    private void pushWaiter() {
+        if (head == null) {
+            head = new Waiter(Thread.currentThread());
+        } else {
+            head = new Waiter(Thread.currentThread(), head);
+        }
+    }
+
+    private Waiter popWaiter() {
+        var current = head;
+        head = head.next;
+        return current;
+    }
 }
--- src/main/java/com/dwrik/mutex/Waiter.java
+++ src/main/java/com/dwrik/mutex/Waiter.java
@@ -1,4 +1,15 @@
 package com.dwrik.mutex;
 
 public class Waiter {
+    Thread thread;
+    Waiter next;
+
+    Waiter(Thread thread) {
+        this.thread = thread;
+    }
+
+    Waiter(Thread thread, Waiter next) {
+        this.thread = thread;
+        this.next = next;
+    }
 }
